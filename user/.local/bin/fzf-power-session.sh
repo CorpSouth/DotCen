b706d19e9b9db2a200a704f41dd216dee5d44c71
Aaron <96737550+CorpSouth@users.noreply.github.com>
2024-08-22 05:07:01 +0000

    Add files via upload

--- user/.local/bin/fzf-power-session.sh
+++ user/.local/bin/fzf-power-session.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+# This allows us to back out gracefully.
+set -e
+
 # We don't want multiple instances of the same exact script.
 pidof -x -o $$ "$(basename "$0")" && exit 1
 
-# Preformat a string to pipe through fzf for the case statement below.
+# Print out session options in a line-broken format and pipe through to fzf.
 MENU="$(printf 'Lock Screen\nLog Out\nPower Off\nReboot\nSuspend' | fzf --border-label='[FZF-Power-&-Session]' --disabled --info='hidden' --no-scrollbar)"
 
-# Preformat a string for a dedicated fzf confirmation prompt.
+# Print out confirmation options in a line-broken format and pipe through to fzf.
 CONFIRM="$(printf 'Yes\nNo' | fzf --border-label='Proceed?' --disabled --info='hidden' --no-scrollbar)"
 
+# If you edit this, make sure to change the variable strings above to reflect your changes.
 case "$MENU" in
                 "Lock Screen" )
                 case "$CONFIRM" in
