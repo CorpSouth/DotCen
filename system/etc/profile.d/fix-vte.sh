6818a9e57117a39b9af90cb0281ac04976ba72ca
Aaron <96737550+CorpSouth@users.noreply.github.com>
2024-09-01 03:36:13 +0000

    Update and rename fix-vte.sh to tilix.sh

--- system/etc/profile.d/fix-vte.sh
+++
@@ -1,6 +0,0 @@
-# check if function exists and define empty one if doesn't
-if [[ $(type -t "__vte_prompt_command") != function ]]; then
-    function __vte_prompt_command(){
-        return 0
-    }
-fi
